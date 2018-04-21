e5d14e906dc6e4a676504ec707bd3de9465bf674
unknown <alyciarileym@gmail.com>
2018-04-20 22:16:58 -0500

    file structure and components.

--- nyscraper/src/App.js
+++ nyscraper/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import Saved from "./components/Saved";
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+          <Header />
+          <Search />
+          <Results />
+          <Saved />
+        </Wrapper>
     );
   }
+
+
+
+
+
 }
 
 export default App;
